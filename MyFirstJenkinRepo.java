a28f0f12ee057e7bd37faad51e48dc438ac4d8c2
Serin Anoopraj <serin.anoopraj@capgemini.com>
2021-12-01 14:18:25 +0530

    JenkinRepo Committed

---
+++ MyFirstJenkinRepo.java
@@ -0,0 +1,10 @@
+public class MyFirstJenkinRepo {
+
+	public static void main(String[] args) {
+	System.out.println("Hello world");
+	System.out.println("This is my jenkin project");
+
+	System.out.println("This is second attempt");
+	}
+
+}
